2f78fc4649bf551de7b1310bd765eb457eda7ce1
Shugo Kawamura <shugo_k@flowing.tokyo>
2020-12-31 17:02:55 +0900

    backup

--- exporter/main.go
+++ exporter/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
+	"sync"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
@@ -88,15 +91,48 @@ func main() {
 			t += fmt.Sprintf("%d / %s / %s / %s\n",i+1,profile.Input[i].OriginalTitle,profile.Input[i].Type,profile.Input[i].Text)
 		}
 		msgbox.SetText(t)
+
+		// Specify destination...
+		directory, err := dialog.Directory().Title("Directory to save assets").Browse()
 		
-		// Replace paths...
+		// goroutine setup
+		wg := sync.WaitGroup{}
+		wg.Add(1)
 		for _,v := range profile.Input {
 			if v.Type == InputMovie || v.Type == InputImage || v.Type == InputTitle {
 				log.Printf("Input Type : %s : Path : %s\n",v.Type,v.Text)
+				go func(v models.Input){
+				
+					// Resolve file paths
+					filename := path.Base(v.Text)
+					// Copy/Save all static assets...
+					source, err := os.Open(v.Text)
+        			if err != nil {
+						log.Println("Failed to read file :",err)
+						return
+					}
+        			defer source.Close()
+
+					destpath := path.Join(directory,filename)
+        			destination, err := os.Create(destpath)
+					if err != nil {
+						log.Println("Failed to make file :",err)
+						return
+					}
+					defer destination.Close()
+					// copy
+					_,err = io.Copy(destination, source); if err != nil {
+						log.Println("Failed to copy file :",err)
+						return
+					}
+				
+					// Replace paths(relative)
+					v.Text = destpath
+
+					// TODO: Save CRC checks
+				}(v)
 			}
 		}
-
-		// Save replaced profile and assets
 	  }),
 	)
    
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/FlowingSPDG/vMix-profile-export
 go 1.15
 
 require (
-	fyne.io/fyne v1.4.2 // indirect
-	github.com/sqweek/dialog v0.0.0-20200911184034-8a3d98e8211d // indirect
+	fyne.io/fyne v1.4.2
+	github.com/sqweek/dialog v0.0.0-20200911184034-8a3d98e8211d
 )
